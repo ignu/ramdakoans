88d5da17e8e38ebc276064fe9b804f745214b4c8
Len Smith <len@barrison.com>
2018-04-03 09:36:09 -0700

    applyTo

--- array.test.js
+++ array.test.js
@@ -71,4 +71,14 @@ describe("applySpec", () => {
   });
 });
 
+describe("applyTo", () => {
+  it("applies a function to a value", () => {
+    const add1 = R.add(1);
+    const applyTo2 = R.applyTo(2);
+
+    const result = applyTo2(add1);
+
+    expect(result).toEqual(3);
+  });
+});
 
