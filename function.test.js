6f1301b1bca78cb2390298cbdcae63352ea92e80
Len Smith <len@barrison.com>
2018-04-03 20:37:07 -0700

    __

--- function.test.js
+++ function.test.js
@@ -1,5 +1,14 @@
 const R = require("ramda");
 
+describe("__", () => {
+  it("acts as a placeholder", () => {
+    const lessThan3 = R.lt(R.__, 3);
+
+    expect(lessThan3(1)).toEqual(true)
+  });
+});
+
+
 describe("applyTo", () => {
   it("applies a function to a value", () => {
     const add1 = R.add(1);
